46aef819a62f1c474da90f556f90d19eb8df612d
Lucas <lmcginnisn01@gmail.com>
2024-01-27 15:35:38 -0500

    removed old json files from 2023, fixed a few bugs, created new spreadsheet, and added 2024 json for our first event

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import statbotics
 import json
 import requests
 import datetime
+import time
 
 constants = open("./json-files/constants.json")
 constant = json.load(constants)
@@ -25,7 +26,7 @@ TBA_BASE_ENDPOINT = "https://www.thebluealliance.com/api/v3"
 gc = gspread.service_account(filename="json-files/credentials.json")
 
 # Open a sheet from a spreadsheet in one go
-sheet = gc.open("6002 ZooBOTix data sheet").sheet1
+sheet = gc.open("6002 DataSheet 2024").sheet1
 print(sheet.url)
 
 api_data_2d = [
@@ -45,14 +46,7 @@ team_names_2d = [
 ]
 
 event_keys = [
-    "2023arc",
-    "2023cur",
-    "2023dal",
-    "2023gal",
-    "2023hop",
-    "2023joh",
-    "2023mil",
-    "2023new",
+    "2024mibat"
 ]
 
 team_epa = [
@@ -71,9 +65,17 @@ team_winrate_2d = [
 
 ]
 
+team_states = [
+
+]
+
+team_states_2d = [
+
+]
+
 gc = gspread.service_account(filename="json-files/credentials.json")
 
-sh = gc.open("6002 ZooBOTix data sheet")
+sh = gc.open("6002 DataSheet 2024")
 worksheet = sh.sheet1
 
 
@@ -137,36 +139,65 @@ def update_team_list(update_sheet):
             "values": team_names_2d
         }])
 
+    for event in event_keys:
+        with open(f"./json-files/{event}.json") as jsonFile:
+            data = json.load(jsonFile)
+            for item in data:
+                team_states.append(item["state_prov"])
+                team_states_2d.append([item["state_prov"]])
+
+    if update_sheet is True:
+        print(len(team_states_2d))
+        sheet.batch_update([{
+            "range": f"{team_state_column}2:{team_state_column}{len(api_data) + 1}",
+            "values": team_states_2d
+        }])
+
 
 def update_epa(update_sheet):
     api_data.sort()
     api_data_2d.sort()
     for team in api_data:
-        team_data = stats.get_team(team)
-        epa = team_data["norm_epa"]
-        rookie_year = team_data["rookie_year"]
-        rookies = datetime.date.today().year == int(rookie_year)
-        if epa is None:
-            epa = "N/A"
+        if team < 9542:
+            team_data = stats.get_team(team)
+            epa = team_data["norm_epa"]
+            rookie_year = team_data["rookie_year"]
+            rookies = datetime.date.today().year == int(rookie_year)
+            if epa is None:
+                epa = "N/A"
+            else:
+                epa = round(epa / 28.1702899, 1)
+                print(f"team: {team}, epa: {epa}, rookies?: {rookies}")
+                team_epa.append(epa)
+                team_epa_2d.append([epa])
         else:
-            epa = round(epa / 28.1702899, 1)
-        print(f"team: {team}, epa: {epa}, rookies?: {rookies}")
-        team_epa.append(epa)
-        team_epa_2d.append([epa])
-    if update_sheet is True:
-        sheet.sort((1, "asc"), range=f"{team_number_column}2:{team_number_column}{len(api_data_2d) + 1}")
-        sheet.batch_update([{"range": f"{team_epa_column}2:{team_epa_column}{len(team_epa_2d) + 1}", "values": team_epa_2d}])
+            team_epa.append(0)
+            team_epa_2d.append([0])
+            print(team)
+
     print(team_epa)
     print(team_epa_2d)
+    if update_sheet is True:
+        sheet.sort((1, "asc"), range=f"{team_number_column}2:{team_number_column}{len(api_data_2d) + 1}")
+        sheet.batch_update([{
+            "range": f"{team_epa_column}2:{team_epa_column}{len(team_epa_2d) + 1}",
+            "values": team_epa_2d
+        }])
 
 
 def update_winrate(update_sheet):
     for team in api_data:
-        winrate = stats.get_team(team)["winrate"]
-        team_winrate.append(f"{round(winrate * 100, 1)}%")
-        team_winrate_rounded = f"{round(winrate * 100, 1)}%"
-        team_winrate_2d.append([team_winrate_rounded])
-        print(f"{round(winrate * 100, 2)}%")
+        if team < 9542:
+            winrate = stats.get_team(team)["winrate"]
+            team_winrate.append(f"{round(winrate * 100, 1)}%")
+            team_winrate_rounded = f"{round(winrate * 100, 1)}%"
+            team_winrate_2d.append([team_winrate_rounded])
+            print(f"{round(winrate * 100, 2)}%")
+        else:
+            team_winrate.append(0)
+            team_winrate_2d.append([0])
+            print(f"error:{team}")
+
     print(team_winrate)
     print(team_winrate_2d)
     if update_sheet is True:
@@ -178,5 +209,5 @@ def update_winrate(update_sheet):
 
 
 update_team_list(update_sheet=True)
-# update_epa(update_sheet=True)
-# update_winrate(update_sheet=True)
+update_epa(update_sheet=True)
+update_winrate(update_sheet=True)
